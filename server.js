3e7a1f19c1e7fd5b81db64eaa39015951fe6000a
taweeporn1994 <taweepornjitaksorn@gmail.com>
2024-05-23 13:24:44 +0700

    init

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const os = require('os');
-const app = express();
-const PORT = 8080;
+const express = require('express')
+const os = require('os')
+const app = express()
+const PORT = 8081
 
 function getHostIP() {
-  const interfaces = os.networkInterfaces();
+  const interfaces = os.networkInterfaces()
   for (const name of Object.keys(interfaces)) {
     for (const iface of interfaces[name]) {
       if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address;
+        return iface.address
       }
     }
   }
-  return 'IP not found';
+  return 'IP not found'
 }
 
+const hostIP = getHostIP()
 app.get('/', (req, res) => {
-  const hostIP = getHostIP();
-  res.send(`Hello, World! Host IP: ${hostIP}`);
-});
+  const clientIP =
+    req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const timestamp = new Date().toISOString()
+  console.log('clientIP|timestamp :', `${clientIP}|${timestamp}`)
+  res.send(`Hello, World! Host IP: ${hostIP}`)
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://${hostIP}:${PORT}`)
+})
